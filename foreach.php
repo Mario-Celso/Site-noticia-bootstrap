0fee5bb553018f0751524707615616e8a4504398
Mario Celso de Almeida Moraes Junior <39558003+Mario-Celso@users.noreply.github.com>
2018-12-04 14:12:58 -0200

    Add files via upload

---
+++ foreach.php
@@ -0,0 +1,14 @@
+<?php
+
+$dados=array("NOME" =>"Maik","IDADE" =>22);
+$endereco=array("RUA" => "emilia" ,"NUMERO" => 516 );
+$maik=array("DADOS" => $dados , "ENDEREÇO" => $endereco );
+
+  foreach ($dados as $key => $value)
+  {
+      foreach ($dados as $key)
+      {
+        echo $dados;
+      }
+  }
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$Maik= array(
+ "nome" => "maik",
+ "idade" => 22,
+ "altura" => 1.2
+);
+
+$Mario= array(
+ "nome" => "Mario",
+ "idade" => 19
+);
+
+$Carlos= array(
+ "nome" => "Carlos",
+ "idade" => 25
+);
+
+$pessoas = array( $Maik , $Mario, $Carlos );
+
+
+  foreach ($Maik as $key)
+   {
+     echo $key. "<br>";
+     echo "<br>";
+
+}
+
+
+     //$j=($i==10)? false : true;
+
+
+
+
+
+
+ ?>
